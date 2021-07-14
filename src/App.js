d954e060f0588b8edd43cebc33d0e519aa76abeb
Rahul <rahulasr101@gmail.com>
2021-07-15 00:46:40 +0530

    Created project base

---
+++ src/App.js
@@ -0,0 +1,29 @@
+const Pet = ({ name, animal, breed }) => {
+  return React.createElement('div', {}, [
+    React.createElement('h1', {}, name),
+    React.createElement('h2', {}, animal),
+    React.createElement('h3', {}, breed),
+  ]);
+};
+
+const App = () => {
+  return React.createElement('div', {}, [
+    React.createElement('h1', {}, 'Adopt Me!'),
+    React.createElement(Pet, {
+      name: 'luna',
+      animal: 'dog',
+      breed: 'Havanese',
+    }),
+    React.createElement(Pet, {
+      name: 'Tommy',
+      animal: 'dog',
+      breed: 'German Sheperad',
+    }),
+    React.createElement(Pet, {
+      name: 'Dsee',
+      animal: 'Cat',
+      breed: 'Mixed',
+    }),
+  ]);
+};
+ReactDOM.render(React.createElement(App), document.getElementById('root'));
