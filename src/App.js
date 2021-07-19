246672f17b4520b312e980575a0ac410707eabfd
Rahul <rahulasr101@gmail.com>
2021-07-20 01:49:31 +0530

    Interated JSX

--- src/App.js
+++ src/App.js
@@ -2,23 +2,13 @@ import React from 'react';
 import { render } from 'react-dom';
 import Pet from './Pet';
 const App = () => {
-  return React.createElement('div', {}, [
-    React.createElement('h1', {}, 'Adopt Me!'),
-    React.createElement(Pet, {
-      name: 'luna',
-      animal: 'dog',
-      breed: 'Havanese',
-    }),
-    React.createElement(Pet, {
-      name: 'Tommy',
-      animal: 'dog',
-      breed: 'German Sheperad',
-    }),
-    React.createElement(Pet, {
-      name: 'Dsee',
-      animal: 'Cat',
-      breed: 'Mixed',
-    }),
-  ]);
+  return (
+    <div>
+      <h1 id='something-important'>Adopt Me!</h1>
+      <Pet name='Luna' animal='Dog' breed='German'></Pet>
+      <Pet name='Alex' animal='Bird' breed='German'></Pet>
+      <Pet name='Jima' animal='Tiger' breed='German'></Pet>
+    </div>
+  );
 };
-render(React.createElement(App), document.getElementById('root'));
+render(<App />, document.getElementById('root'));
