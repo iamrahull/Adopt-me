eff7b7bdc2d13ff367f4f620a8785e133ce2371b
Rahul <rahulasr101@gmail.com>
2021-09-22 02:30:51 +0530

    added custom hook to remove code duplication

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import { render } from 'react-dom';
-import Pet from './Pet';
+import SearchParams from './searchParams';
 const App = () => {
   return (
     <div>
       <h1 id='something-important'>Adopt Me!</h1>
-      <Pet name='Luna' animal='Dog' breed='German'></Pet>
-      <Pet name='Alex' animal='Bird' breed='German'></Pet>
-      <Pet name='Jima' animal='Tiger' breed='German'></Pet>
+      <SearchParams />
     </div>
   );
 };
---
+++ src/searchParams.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import { ANIMALS } from '@frontendmasters/pet';
+import useDropdown from './useDropdown';
+const SearchParams = () => {
+  const [location, setLocation] = useState('Seatlle, WA');
+  const [breeds, setBreeds] = useState([]);
+  const [animal, AnimalDropdown] = useDropdown('Animal', 'Dog', ANIMALS);
+  const [breed, BreedDropdown] = useDropdown('Breed', '', breeds);
+
+  return (
+    <div className='search-params'>
+      <form>
+        <label htmlFor='location'>
+          location
+          <input
+            id='location'
+            value={location}
+            placeholder='Location'
+            onChange={event => setLocation(event.target.value)}
+          />
+        </label>
+        <AnimalDropdown />
+        <BreedDropdown />
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+};
+export default SearchParams;
